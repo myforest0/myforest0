5aa7935722aedbd0dd5c5a1bcc0e29b5b43077f1
xiangshilin <19938570370@189.cn>
2024-01-19 21:44:48 +0800

    修改首页样式

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
         nav,
         footer: {
             message: 'Released under the MIT License.',
-            copyright: '<a href="https://beian.miit.gov.cn/">蜀ICP备2023020990号-1</a> Copyright © 2019-present Forest 我的键修飞升指南'
+            copyright: '<a href="https://beian.miit.gov.cn/">蜀ICP备2023020990号-1</a> Copyright © 2019-present Forest'
         },
         sidebar: {
             // 当用户在 `mds` 目录页面下将会展示这个侧边栏
